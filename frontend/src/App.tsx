4c11d087bf7a9b1ae32c44c6abbc759c2710e4fb
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-09-24 22:32:05 +0800

    feat: add user registration form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,44 @@
-const App = () => (
-  <div>
-    Working
-  </div>
-)
+import React, { useState } from 'react'
+import userService from './services/user.ts'
+
+const App = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const addUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const userObject = {
+      name, 
+      email, 
+      password
+    }
+
+
+    const user = await userService.create(userObject)
+    setName('')
+    setEmail('')
+    setPassword('')
+    alert(`${user.name} registered successfully`)
+  }
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={addUser}>
+        <div>
+          name <input type='text' value={name} onChange={({target}) => setName(target.value)} />
+        </div>
+        <div>
+          email <input type='email' value={email} onChange={({target}) => setEmail(target.value)} />
+        </div>
+        <div>
+          password <input type='password' value={password} onChange={({target}) => setPassword(target.value)} />
+        </div>
+        <button type='submit'>register</button>
+      </form>
+    </div>
+  )
+}
 
 export default App
